9a464bdaf1ede2a1962328d6d3acec28ae85b31c
RiyaadHossain <riyadhossain.dev@gmail.com>
2022-03-31 11:41:30 +0600

    Customize hooks[useCart, useProducts] added

---
+++ src/components/Hooks/useCart.js
@@ -0,0 +1,21 @@
+import { useEffect, useState } from "react"
+import { getStoredCart } from "../../utilities/fakedb"
+
+const useCart = (products) => {
+    const [cart, setCart] = useState([])
+    useEffect(() => {
+        const storedCart = getStoredCart()
+        const newCart = []
+        for (const id in storedCart) {
+            const previousProduct = products.find(product => product.id === id)
+            if (previousProduct) {
+                previousProduct.quaintity = storedCart[id]
+                newCart.push(previousProduct)
+            }
+        }
+        setCart(newCart)
+    }, [products])
+    return [cart, setCart]
+}
+
+export default useCart
--- src/components/Orders/Orders.jsx
+++ src/components/Orders/Orders.jsx
@@ -1,9 +1,18 @@
 import React from 'react';
+import useCart from '../Hooks/useCart';
+import useProducts from '../Hooks/useProducts';
 
 const Orders = () => {
+    const [products, setProducts] = useProducts()
+    const [cart, setCart] = useCart(products)
+    let count = 0;
+    for (const item of cart) {
+        count = item.quaintity + count
+    }
     return (
         <div>
-            <h1>This is Orders</h1>
+            <h1>This is Orders: {products.length}</h1>
+            <h2>Cart: {count}</h2>
         </div>
     );
 };
